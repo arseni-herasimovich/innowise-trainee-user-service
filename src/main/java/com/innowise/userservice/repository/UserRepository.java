a4270ea1e3536ea56afaa00a2d2358450d0da135
arseni-herasimovich <arseni.herasimovich@innowise.com>
2025-10-29 13:03:58 +0300

    fix: change "findAll with pagination" logic to resolve n+1 problem and still using pageable

--- src/main/java/com/innowise/userservice/repository/UserRepository.java
+++ src/main/java/com/innowise/userservice/repository/UserRepository.java
@@ -1,16 +1,36 @@
 package com.innowise.userservice.repository;
 
 import com.innowise.userservice.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findUserByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    /*
+        We are not able to use pageable with entity graph (we need to resolve n+1)
+        So we get user ids firstly and then fetch users with cards
+     */
+
+    @Query("select u.id from User u")
+    Page<UUID> findUserIds(Pageable pageable);
+
+    @EntityGraph(attributePaths = {"cards"})
+    @Query("select u from User u where u.id in :ids")
+    List<User> findAllWithCardsByIds(@Param("ids") List<UUID> ids);
 
     // redundant methods only to follow the task requirements
 
@@ -31,6 +51,4 @@ public interface UserRepository extends JpaRepository<User, UUID> {
             WHERE id = :id
             """, nativeQuery = true)
     void delete(UUID id);
-
-    boolean existsByEmail(String email);
 }
