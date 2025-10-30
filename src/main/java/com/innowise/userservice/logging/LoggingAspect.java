7b400f96e1aa845637bed7e56e9af5afaa48e134
arseni-herasimovich <arseni.herasimovich@innowise.com>
2025-10-30 19:25:56 +0300

    fix: change logging aspect logic

--- src/main/java/com/innowise/userservice/logging/LoggingAspect.java
+++ src/main/java/com/innowise/userservice/logging/LoggingAspect.java
@@ -18,16 +18,12 @@ public class LoggingAspect {
 
     @Around("serviceLayer()")
     public Object log(ProceedingJoinPoint pjp) throws Throwable {
-        if (!log.isDebugEnabled()) {
-            return pjp.proceed();
-        }
-
         var method = pjp.getSignature().toShortString();
         var start = System.nanoTime();
-        log.debug("Entering method: {}", method);
+        log.trace("Entering method: {}", method);
 
         Object result = pjp.proceed();
-        log.info("Method: {} executed. Time of execution: {} ms", method, (System.nanoTime() - start) / 1000000);
+        log.debug("Method: {} executed. Time of execution: {} ms", method, (System.nanoTime() - start) / 1000000);
         return result;
     }
 }
