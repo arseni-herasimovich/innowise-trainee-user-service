125cdfb8f16e7f23296792b9c98a644714694361
arseni-herasimovich <arseni.herasimovich@innowise.com>
2025-11-07 16:31:25 +0300

    feat: service layer
    * feat: create dto's for User and Card entities
    
    * feat: implement mappers that map entities to dto and dto to entities
    
    * feat: create custom extensions
    
    * feat: create services
    
    * feat: implement services
    
    * feat: implement logging
    
    * fix: define mapper's component model using enum
    
    * feat: create exceptions for already existing user email and card number
    
    * fix: do not catch exceptions in logging aspect
    
    They will be caught by exception handler.
    
    * fix: check if user email or card number already exists, set userId in cards
    
    * fix: fix update logic
    
    * fix: add cards list to dto, make it fetchable in findAll and findBy requests
    
    * fix: wrong transaction annotation
    
    * fix: remove sensitive data from logging
    
    * fix: ignore cards in mapper
    
    * fix: change mappers to correctly define mapping targets
    
    * fix: correctly define total elements of page
    
    * fix: change usermapper's mapping annotations
    
    * fix: change logging aspect logic
    
    * fix: rewrite logging logic with dividing into two profiles, added custom exception for catching errors with data, using new type of expirationDate of Card
    
    * fix: remove redundant imports
    
    * feat: deserialize LocalDate in card create request using custom deserializer
    
    * Delete src/main/resources/README.md

---
+++ src/main/java/com/innowise/userservice/mapper/UserMapper.java
@@ -0,0 +1,19 @@
+package com.innowise.userservice.mapper;
+
+import com.innowise.userservice.dto.UserCreateRequest;
+import com.innowise.userservice.dto.UserResponse;
+import com.innowise.userservice.dto.UserUpdateRequest;
+import com.innowise.userservice.entity.User;
+import org.mapstruct.*;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, uses = CardMapper.class)
+public interface UserMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "cards", ignore = true)
+    User toEntity (UserCreateRequest request);
+    UserResponse toResponse(User user);
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "cards", ignore = true)
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void update(UserUpdateRequest request, @MappingTarget User user);
+}
