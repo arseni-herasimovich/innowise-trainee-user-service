b0169e5a922757abe671388b3faf91e455ed2c92
arseni-herasimovich <arseni.herasimovich@innowise.com>
2025-11-07 16:45:11 +0300

    Revert "feat: service layer" (#10)
    This reverts commit 125cdfb8f16e7f23296792b9c98a644714694361.

--- src/main/java/com/innowise/userservice/exception/CardNumberAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package com.innowise.userservice.exception;
-
-import org.springframework.http.HttpStatus;
-
-public class CardNumberAlreadyExistsException extends ApiException {
-    public CardNumberAlreadyExistsException(String number) {
-        super("Card with this number: " + number + " already exists!", HttpStatus.CONFLICT);
-    }
-}
--- src/main/java/com/innowise/userservice/exception/UserEmailAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package com.innowise.userservice.exception;
-
-import org.springframework.http.HttpStatus;
-
-public class UserEmailAlreadyExistsException extends ApiException {
-    public UserEmailAlreadyExistsException(String email) {
-        super("User with email " + email + " already exists!", HttpStatus.CONFLICT);
-    }
-}
