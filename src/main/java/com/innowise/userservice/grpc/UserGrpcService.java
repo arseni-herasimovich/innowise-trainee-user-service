04533e9db04cc28a8c9e92f22510432c272804cd
arseni-herasimovich <arseni.herasimovich@innowise.com>
2025-12-05 01:51:16 +0300

    feat: gRPC Service
    * feat: implement eureka
    
    * fix: id is now required for user creating
    
    * feat: add spring security, create jwt filter with validating token through auth-service, create security config with exception handling
    
    * fix: docker compose
    
    * feat: implement RBAC in all endpoints
    
    * fix: improve unit tests
    
    * fix: integration tests extends from one common abstract class, mockMvc instead of testRestTemplate (to avoid filters)
    
    * fix: create AuthenticationManager bean
    
    * feat: implement security tests
    
    * fix: can create card method now always throws exception with explanation of the reasons for access denial
    
    * fix: jwt filter now continues chain without authentication if response from auth service is null (instead of throwing runtime exception)
    
    * fix: handler for AccessDeniedExceptions now shows full message
    
    * feat: grpc service for creating a user

---
+++ src/main/java/com/innowise/userservice/grpc/UserGrpcService.java
@@ -0,0 +1,42 @@
+package com.innowise.userservice.grpc;
+
+import com.innowise.userservice.generated.User;
+import com.innowise.userservice.generated.UserServiceGrpc;
+import com.innowise.userservice.exception.UserAlreadyExistsException;
+import com.innowise.userservice.mapper.UserGrpcMapper;
+import com.innowise.userservice.service.UserService;
+import io.grpc.Status;
+import io.grpc.stub.StreamObserver;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+@RequiredArgsConstructor
+@Slf4j
+public class UserGrpcService extends UserServiceGrpc.UserServiceImplBase {
+    private final UserService userService;
+    private final UserGrpcMapper userGrpcMapper;
+
+    @Override
+    public void createUser(User.UserCreateRequest request, StreamObserver<User.UserResponse> responseObserver) {
+        log.debug("Received request to create user with id: {}", request.getId());
+        try {
+            var response = userService.create(
+                    userGrpcMapper.toRequest(request)
+            );
+            log.debug("User with id {} created successfully", response.id());
+            responseObserver.onNext(userGrpcMapper.toResponse(response));
+            responseObserver.onCompleted();
+        } catch (UserAlreadyExistsException e) {
+            log.debug("User with id {} already exists", request.getId());
+            var status = Status.ALREADY_EXISTS.withDescription(e.getMessage()).asRuntimeException();
+            responseObserver.onError(status);
+        }
+        catch (Exception e) {
+            log.error("Error while creating user", e);
+            var status = Status.UNKNOWN.withDescription("Internal server error").asRuntimeException();
+            responseObserver.onError(status);
+        }
+    }
+}
